dcb9decc9e8c8bf35131a9a8118b98ff2e9f2a32
moonlab <info@moonlab.com.au>
2016-07-03 23:37:04 +1000

    Completed Project Codding

--- js/common/dataFactory.js
+++ js/common/dataFactory.js
@@ -1,26 +1,16 @@
 (function(){
-    var dataFactory = function($http){
-        var factory = {};
+    var dataFactory = function($http, $log){
+        var factory = {},keyword = null;
         var url = "https://gateway-a.watsonplatform.net/calls/text/TextGetEmotion?apikey=";
         var APIkey = "4d395301e1cf0abcae8548dfa07cfdad2b96f24c";
         var confParam = "&outputMode=json&showSourceText=1&text=";
-//         return {
-//          getText: function(callback){
-//            $http.get('asserts/docs/henry_iv.json').success(callback);
-//          },
-//          getEmotions: function(callback, keyword){
-//            keyword = decodeURI(keyword)
-//            url = url + APIkey + confParam + keyword;
-//            $http.get(url).
-//            success(callback).
-//            error(function(data, status, headers, config) {
-//              console.log('error')
-//            });
-//         }
-//        };
-//        
-        factory.getText = function(){
-            return $http.get('asserts/docs/henry_iv.json');
+      
+        factory.getText = function(callback){
+            return $http.get('asserts/docs/henry_iv.json').success(callback)
+            .error(function(data, status, headers, config){
+                //handle errors
+                $log.log(data.error + ' Status ' + status);
+            });
         };
         
         factory.getEmotions = function(keyword){
@@ -33,9 +23,7 @@
         return factory;    
     };
             
-    
-    
-    dataFactory.$inject = ['$http']
+    dataFactory.$inject = ['$http', '$log']
     
 
     angular.module('EmotionApp').factory('dataFactory', dataFactory);
--- js/modules/playText/playTextController.js
+++ js/modules/playText/playTextController.js
@@ -1,38 +1,34 @@
 (function(){
 
-    var playTextCtrl = function ($scope, dataFactory) {
+    var playTextCtrl = function ($scope, $log, dataFactory) {
         $scope.textData = null; 
         $scope.score = null;
-        var keyword = null;
-//        $scope.getPlayData = dataFactory.getText(function(textData) {
-//          $scope.textData = textData;
-//        });
-        $scope.getPlayData = dataFactory.getText()
-            .success(function(textData){
-                    $scope.textData = textData;
-                
-            })
-            .error(function(data, status, headers, config){
-                //handle errors
-                $log.log(data.error + ' Status ' + status);
+        keyword = null,text = null;
+        
+        $scope.getPlayData = function(){
+            dataFactory.getText(function(textData) {
+              $scope.textData = textData;
             });
-        $scope.getEmotions = dataFactory.getEmotions(keyword)
+        }
+
+        $scope.getEmotionsAnalysis = function(text){
+            $scope.score = "";
+            keyword = text.text_entry;
+            dataFactory.getEmotions(keyword)
             .success(function(score){
-                    $scope.score = score;
-                $log.log(data.error + ' score ' + score);
+                $scope.score = score;
+                
                 
             })
             .error(function(data, status, headers, config){
                 //handle errors
                 $log.log(data.error + ' Status ' + status);
             });
-//        $scope.getEmotionsAnalysis =  dataFactory.getEmotions(function(score, text) {
-//            console.log(text);
-//          $scope.score = score;
-//        });
+        }
+
     };
         
-    playTextCtrl.$inject = ['$scope','dataFactory'];
+    playTextCtrl.$inject = ['$scope','$log','dataFactory'];
 
     angular.module('EmotionApp').controller('playTextCtrl',playTextCtrl);
 
